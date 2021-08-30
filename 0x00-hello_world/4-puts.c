4208323fadad9dc5f413c0da0eabb1f894a782ae
sablade <abladesteven@hotmail.com>
2021-08-30 14:44:33 +0000

    Low Level Programming Basics. Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 /**
- *main - Main function of my project
- *
- *
- *Return: return zero everytime
+ * main - My Main Block
+ * Return : 0 everytime
  */
-int main(void)
+int main (void)
 {
-
-	char displayed[52];
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }

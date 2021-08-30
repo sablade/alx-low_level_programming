447395f5850a7a79b0e45a62df2b40ff5af1ab50
sablade <abladesteven@hotmail.com>
2021-08-30 14:46:51 +0000

    Low Level Programming Basics. Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - My Main Block
  * Return : 0 everytime
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }

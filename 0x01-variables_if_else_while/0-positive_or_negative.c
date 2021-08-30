94584d6cba75f3ff539ca61f3a8133a5bb9ea668
sablade <abladesteven@hotmail.com>
2021-08-30 21:26:41 +0000

    Variables and If Else Statements Basics

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Main block for code implementation
+ * Return - return value of 0 everytime
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{
+		printf("%d is positive.\n", n);
+	}
+	else
+	{
+	if(n == 0)
+	{
+		printf("%d is zero.\n", n);
+	}
+	else
+	{
+		printf("%d is negative.\n", n);
+	}
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+#Variables, If and If Else
+** Getting Used To Coding In C Using The Vi Editor

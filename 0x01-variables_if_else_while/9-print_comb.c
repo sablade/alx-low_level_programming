be3a665b0b6157650d809d3f9f8c49741bbde0d2
sablade <abladesteven@hotmail.com>
2021-08-31 04:25:34 +0000

    Variables and If Else Statements Basics

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print single digit number combination with spaces and commas.
+ * Return - return 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

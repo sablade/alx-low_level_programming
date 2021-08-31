f87a5ae4d300fad8cb63077c5ab576df611f0a12
sablade <abladesteven@hotmail.com>
2021-08-31 02:43:01 +0000

    Variables and If Else Statements Basics

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main block for code implementation
+ * Return - return 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch='a'; ch<='z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}

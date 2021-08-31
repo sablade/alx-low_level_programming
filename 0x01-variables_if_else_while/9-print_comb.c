6bf737fca2c4f57b3df2519f1a9daf6e3fc87ac8
sablade <abladesteven@hotmail.com>
2021-08-31 04:35:16 +0000

    Variables and If Else Statements Basics

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '8'; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 		if (num == '9')
@@ -17,6 +17,6 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }

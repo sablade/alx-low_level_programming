0be3629732027bb239d6ec641be770f3b936a9c3
sablade <abladesteven@hotmail.com>
2021-09-01 22:33:03 +0000

    Functions and Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,15 @@
+#include <main.h>
+/**
+ * print_alphabet - Function to print out lower case alphabets
+ * Return: Returns zero for success
+ */
+void print_alphabet(void)
+{
+	int ch;
 
+	for (i = 'a'; i <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}

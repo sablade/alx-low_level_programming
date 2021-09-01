062b2021ca2ca2232fc84b1ea22ca9a7792b3861
sablade <abladesteven@hotmail.com>
2021-09-01 19:18:07 +0000

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Print The Required Text Out
+ * Return: Return 0 on succes
+ */
+int main(void)
+{
+	char [10] = "_putchar"
+	
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

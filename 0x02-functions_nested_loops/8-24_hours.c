03140ad67a2b999d6c8bc464533e13a093a7fd34
sablade <abladesteven@hotmail.com>
2021-09-02 01:15:23 +0000

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: returns nothing
+ */
+void jack_bauer(void)
+{
+	int m,h;
+
+	for (h = 0; h < 24; h++)
+	{
+	for (m = 0; m < 60; m++)
+	{
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0')
+	}
+	}
+}

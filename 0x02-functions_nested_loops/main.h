170d3b9c7b28799c28ec7bd6efcba800f2c0af91
sablade <abladesteven@hotmail.com>
2021-09-02 00:01:36 +0000

    Functions and Nested Loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int _printsign(int n);
+int _print_sign(int n);
 int _abs(int);
 int _print_last_digit(int);
 void jack_bauer(void);

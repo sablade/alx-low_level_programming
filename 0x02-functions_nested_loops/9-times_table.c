8d7be801190584351c2f9ee83024440526dfd71d
sablade <abladesteven@hotmail.com>
2021-09-02 01:44:08 +0000

    Functions and Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,19 @@ void times_table(void)
 	for (ctr = 0; ctr < 10; ctr++)
 	{
 		rslt = num * ctr;
-		_putchar(rslt + '0');
-		_putchar(',');
-		_putchar(' ');
+		if (rslt < 10)
+		{
+			_putchar(rslt + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar((rst / 10) + '0');
+			_putchar((rst % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 		_putchar('\n');
 	}

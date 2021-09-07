843c90feb00294e8a7db4f21885f44a2d7676e34
sablade <abladesteven@hotmail.com>
2021-09-07 01:37:11 +0000

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_array - print n element of array
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 void swap_char(char *a, char *b);
 int _putchar(char c);
 void reset_to_98(int *n);

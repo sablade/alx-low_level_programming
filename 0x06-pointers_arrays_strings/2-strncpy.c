2c782262a851d872b75fbcd5b06a6a452c3ca504
sablade <abladesteven@hotmail.com>
2021-09-09 22:54:19 +0000

    More Pointers, Arrays, Strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,38 +1,22 @@
 #include "main.h"
 /**
- * _strcmp - Function which compare two strings and
- * @s1: first string
- * @s2:second string
- * Return: Returns the difference
- * returns zero if s1 == s2
- * returns negative number if s1 < s2
- * returns positive number if s1 > s2
+ * _strncpy - C function that copies a string, including the
+ * terminating null byte, using at most an inputted number of bytes.
+ * If the length of the source string is less than the maximum byte number,
+ * the remainder of the destination string is filled with null bytes.
+ * Works identically to the standard library function `strncpy`.
+ * @dest: buffer storing the string copy
+ * @src:the source string
+ * @n:max nummber of byte copied
+ * Return: returns
  */
-int _strcmp(char *s1, char *s2)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, diff = 0;
+	int i;
 
-	while (1)
-	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			break;
-		else if (s1[i] == '\0')
-		{
-			diff = s2[i];
-			break;
-		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s1[i] != s2[i])
-		{
-			diff = s1[i] - s2[i];
-			break;
-		}
-			else
-			i++;
-	}
-	return (diff);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }

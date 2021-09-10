577b2f64396997f8fa4e9d9da8309024c1115750
sablade <abladesteven@hotmail.com>
2021-09-10 13:30:47 +0000

    More Pointers, Arrays, Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,8 +24,9 @@ char *cap_string(char *str)
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
 		str[index - 1] == '{' ||
-		str[index - 1] == '}' || index == 0)
+		str[index - 1] == '}')
 			str[index] -= 32;
+		index++;
 	}
 	return (str);
 }

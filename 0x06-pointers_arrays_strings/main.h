eed0ee2d6113ef0f52005e19b5ec4c315ec38b7a
Ablade Steven <abladesteven@hotmail.com>
2021-09-11 02:47:57 +0000

    More Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,6 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+int isPrintableASCII(int n);
+void printHexes(char *b, int start, int end);
+void printASCII(char *b, int start, int end);

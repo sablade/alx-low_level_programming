3ca3b503e458eac7ebad8b646f2b615a348f24ba
sablade <abladesteven@hotmail.com>
2021-09-17 20:59:10 +0000

    Argc and Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,17 @@
  */
 int main (int argc, char *argv[])
 {
-	(void) argc;
+	int mul;
 
-	if (argv[1] && argv[2])
+	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * argv[2]);
-		return (0);
+		mul = atoi(argv[1]) * argv[2]
+		printf("%d\n", mul);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }

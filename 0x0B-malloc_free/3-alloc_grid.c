dd04a99ca7a75e3c50c0564182dd0bbc226b7642
sablade <abladesteven@hotmail.com>
2021-09-22 03:12:38 +0000

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,22 +13,22 @@ int **alloc_grid(int width, int height)
 	tab = malloc(sizeof(*tab) * height);
 	if (width <= 0 || height <= 0 || tab == 0)
 		return (NULL);
-	else
+	/*else*/
+	/*{*/
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
+		tab[i] = malloc(sizeof(**tab) * width);
+		if (tab[i] == 0)
 		{
-			tab[i] = malloc(sizeof(**tab) * width);
-			if (tab[i] == 0)
-			{
-				/*Free everything if malloc fails*/
-				while (i--)
-					free(tab[i]);
-				free(tab);
-				return (NULL);
-			}
-			for (j = 0; j < width; j++)
-				tab[i][j] = 0;
+			/*Free everything if malloc fails*/
+			while (i--)
+				free(tab[i]);
+			free(tab);
+			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			tab[i][j] = 0;
 	}
+	/*}*/
 	return (tab);
 }

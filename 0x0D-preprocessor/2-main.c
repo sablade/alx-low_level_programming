530f086f0d1f29aac94a867118b4844f91041111
sablade <abladesteven@hotmail.com>
2021-09-27 23:41:23 +0000

    PreProcessor and Macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints the name of the file it is compiled from
+ * Return: Return void
+ */
+int main (void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}

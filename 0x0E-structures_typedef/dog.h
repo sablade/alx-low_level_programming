5f49700a9de67bb50f6ff6e255b185aaea58f902
sablade <abladesteven@hotmail.com>
2021-09-29 01:13:59 +0000

    Structures and TypeDefs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - structure definition for a dog
+ * struct dog - structure definition of a dog
  * @name: character string
- * @age: int 
+ * @age: integer
  * @owner: character string
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef fpr struct dog
- */
-typdef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

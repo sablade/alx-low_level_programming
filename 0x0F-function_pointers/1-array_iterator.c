eb12d55d271abe871dae96e57d6782b74a65f276
sablade <abladesteven@hotmail.com>
2021-10-01 14:24:00 +0000

    Function Pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL && action != NULL)
-	{
-		for (i = 0; i < size; i++)
-			action(array[i]);
-	}
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }

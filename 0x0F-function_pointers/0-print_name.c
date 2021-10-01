fa0c282ef2b87da22443e3e632121605e748f9ea
sablade <abladesteven@hotmail.com>
2021-10-01 12:29:17 +0000

    Function Pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints name from a funtcion pointer
+ * @name: string to print out
+ * @f: function pointer with string argument
+ */
+void print_name(char *name, void (*f)(*char))
+{
+	if (f != NULL )
+		f(name);
+}
---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,11 @@
+#ifndef THREE_CALC_H
+#define THREE_CALC_H
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif

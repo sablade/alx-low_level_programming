84911191d71c39d3a285d3e181b1a4a5ddf18b5f
sablade <abladesteven@hotmail.com>
2021-10-27 23:37:49 +0000

    File I/O Content Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,40 +10,30 @@
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, f_write, f_close;
-	ssize_t f_read;
+	ssize_t f_read = SIZE;
 	char buf[SIZE];
 
 	if (argc != 3)/** Check argument count while running script **/
-	{
-		dprintf(SE, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	{dprintf(SE, "Usage: cp file_from file_to\n");
+		exit(97); }
 	file_from = open(argv[1], O_RDONLY);/** Open file_from to read **/
 	if (file_from == -1)/** Check for file opening/reading errors **/
-	{
-		dprintf(SE, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	{dprintf(SE, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)/** Check for file writing errors **/
-	{
-		dprintf(SE, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while (f_read > 0)
+	{dprintf(SE, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
+	while (f_read == SIZE)
 	{
 		f_read = read(file_from, buf, SIZE);
 		if (f_read == -1)
-		{
-			dprintf(SE, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+		{dprintf(SE, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
 		f_write = write(file_from, buf, SIZE);
 		if (f_write == 1)
-		{
-			dprintf(SE, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+		{dprintf(SE, "Error: Can't write to %s\n", argv[2]);
+			exit(99); }
 	}
 	f_close = close(file_from);/** Close File Read From **/
 	if (f_close == -1)

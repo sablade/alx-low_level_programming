6484d6acab9f41731446c6a7bbe8bc05fcabe0d0
sablade <abladesteven@hotmail.com>
2021-11-26 02:42:43 +0000

    Doubly Linked Lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_dlistint_t - Prints all the elements in a doubly linked list
+ * print_dlistint - Prints all the elements in a doubly linked list
  * @h: Pointer to Head
  * Return: Returns number of nodes
  */

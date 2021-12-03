8b21badee00ad43b1009209aa4c6c861e80c17d8
Ablade Steven <abladesteven@hotmail.com>
2021-12-03 18:40:39 +0000

    C Dynamic Libraries

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *a = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (a);
+}
